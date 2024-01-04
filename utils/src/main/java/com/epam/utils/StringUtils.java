41ac69cf7bcc554d0b5c6a7b7bfc409c004d2db8
Artur Semenas <uncelfuka@gmail.com>
2024-01-04 11:54:09 +0100

    Stage 2 Module #1. Task #2. Build Tools. Maven

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null || str.length()<1 || str.charAt(0)=='-'){
+            return false;
+        }
+        double num;
+        try {
+            num = Double.parseDouble(str);
+        }catch (NumberFormatException e){
+            return false;
+        }
+        return num>0;
     }
 }
